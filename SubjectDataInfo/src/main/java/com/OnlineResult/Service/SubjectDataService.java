422cf47725c19fbb947b3e3475fdb0c7aa357237
Akash2141 <asgupta2141@gmail.com>
2019-03-08 11:26:21 +0530

    Online Result Maker

--- SubjectDataInfo/src/main/java/com/OnlineResult/Service/SubjectDataService.java
+++ SubjectDataInfo/src/main/java/com/OnlineResult/Service/SubjectDataService.java
@@ -1,5 +1,6 @@
 package com.OnlineResult.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,26 +14,42 @@ import com.google.gson.reflect.TypeToken;
 
 @Service
 public class SubjectDataService {
-	
+
 	@Autowired
 	SubjectDataRepo subjectDataRepo;
-	
-	public void addSubjectData(SubjectDataModel subjectDataModel)
-	{
+
+	public void addSubjectData(SubjectDataModel subjectDataModel) {
 		Gson gson = new Gson();
 		String json = gson.toJson(subjectDataModel);
 		SubjectData examMarks = gson.fromJson(json, SubjectData.class);
 		subjectDataRepo.save(examMarks);
 	}
-	
-	public List<SubjectDataModel> getAllSubjectData()
-	{
+
+	public List<SubjectDataModel> getAllSubjectData() {
 		Iterable<SubjectData> itSubjectData = subjectDataRepo.findAll();
 		Gson gson = new Gson();
 		String json = gson.toJson(itSubjectData);
 		List<SubjectDataModel> listSubjectDataModel = gson.fromJson(json, new TypeToken<List<SubjectDataModel>>() {
 		}.getType());
 		return listSubjectDataModel;
+	}
+
+	public List<SubjectDataModel> getSubjectDataByCalendarYear(String semester, int calendarYear) {
+		Iterable<SubjectData> itSubjectData = subjectDataRepo.findAll();
+		Gson gson = new Gson();
+		String json = gson.toJson(itSubjectData);
+		List<SubjectDataModel> listSubjectDataModel = gson.fromJson(json, new TypeToken<List<SubjectDataModel>>() {
+		}.getType());
+		List<SubjectDataModel> newListSubjectDataModel = new ArrayList<SubjectDataModel>();
+		for(SubjectDataModel subjectModel:listSubjectDataModel)
+		{
+			if(subjectModel.getSemester().equals(semester) & subjectModel.getYear()==calendarYear & subjectModel.getSemester()!=null)
+			{
+				newListSubjectDataModel.add(subjectModel);
+			}
+		}
+
+		return newListSubjectDataModel;
 	}
 
 }
